d8683787b54453631cc5918daf4f0cd980d78604
thestupidconference <stupidconference@ya.ru>
2018-11-20 13:34:16 +0300

    auto commit

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def cmd_help(bot, update):
 
 
 def cmd_pasta(bot, update):
-    update.message.reply_text(text='Сделай сам. Изменения в репозиториии этого бота на гитхабе автоматом посылаются в хероку.',
+    update.message.reply_text(text='Сделай сам. Изменения в репозитории этого бота на гитхабе автоматом посылаются в хероку.',
                               disable_notification=True,
                               parse_mode='html',
                               quote=True)
